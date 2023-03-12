a9ce09245b1ff8c7104c86738f61cf9785546219
Claudio Cocciarelli <oidualc@users.noreply.github.com>
2023-03-12 11:55:26 +0100

    apply node-fetch polyfill

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,6 +1,12 @@
 import "@testing-library/jest-dom";
 
+import nodeFetch from "node-fetch";
 import { server } from "./mocks/server";
+
+//@ts-ignore
+global.fetch = nodeFetch;
+//@ts-ignore
+global.Request = nodeFetch.Request;
 
 beforeAll(() => server.listen());
 afterEach(() => server.resetHandlers());
