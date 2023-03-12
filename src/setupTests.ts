c8a288fe07095b5d34e3e16d912b969d17925c21
Claudio Cocciarelli <oidualc@users.noreply.github.com>
2023-03-12 19:00:39 +0100

    fix vitest error by properly including node-fetch in setupTests.ts file

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,6 +1,16 @@
 import "@testing-library/jest-dom";
 
 import { server } from "./mocks/server";
+import fetch, { Headers, Request, Response } from "node-fetch";
+
+// @ts-ignore
+globalThis.fetch = fetch;
+// @ts-ignore
+globalThis.Headers = Headers;
+// @ts-ignore
+globalThis.Request = Request;
+// @ts-ignore
+globalThis.Response = Response;
 
 beforeAll(() => server.listen());
 afterEach(() => server.resetHandlers());
